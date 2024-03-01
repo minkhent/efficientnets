9db440db92c4ce75d2bf544bf82cd9205d503e98
mk <minkhent.dev@gmail.com>
2024-03-01 10:50:37 +0630

    init commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="efficientCV",
-    version="0.0.1",
+    version="0.0.3",
     author="Min Khent",
     author_email="minkhent.dev@gmail.com",
     description="Computational efficient models for computer vision",
